2fdedb942410876d3941c7a996b5a9e4950bc8ca
Aaron-d-k <aaron.dominic.k.2007@gmail.com>
2026-01-18 02:14:43 +0530

    working app!

--- src/displayapp/screens/CASim.cpp
+++ src/displayapp/screens/CASim.cpp
@@ -1,14 +1,116 @@
 #include "displayapp/screens/CASim.h"
+#include "CASim.h"
+#include <climits>
+
 
 using namespace Pinetime::Applications::Screens;
 
-CASim::CASim() {
-  lv_obj_t* title = lv_label_create(lv_scr_act(), nullptr);
-  lv_label_set_text_static(title, "My test application");
+CASim::CASim(System::SystemTask* systemTask) : wakeLock(*systemTask) {
+  title = lv_label_create(lv_scr_act(), nullptr);
+  lv_label_set_text_static(title, "Fear.");
   lv_label_set_align(title, LV_LABEL_ALIGN_CENTER);
   lv_obj_align(title, lv_scr_act(), LV_ALIGN_CENTER, 0, 0);
+
+
+  canvas = lv_canvas_create(lv_scr_act(), nullptr);
+  drawbuff.fill(0);
+  lv_canvas_set_buffer(canvas, drawbuff.data(), Wcanvas, Hcanvas, LV_IMG_CF_INDEXED_2BIT);
+  lv_canvas_set_palette(canvas, 0, LV_COLOR_BLACK);
+  lv_canvas_set_palette(canvas, 1, LV_COLOR_ORANGE);
+  lv_canvas_set_palette(canvas, 2, LV_COLOR_ORANGE);
+  lv_canvas_set_palette(canvas, 3, LV_COLOR_YELLOW);
+  lv_obj_align(canvas, lv_scr_act(), LV_ALIGN_IN_BOTTOM_MID, 0, -90);
+
+  taskRefresh = lv_task_create(RefreshTaskCallback, 200, LV_TASK_PRIO_LOW, this);
+  wakeLock.Lock(); //released automatically in destructor.
+  tickrest=10;
+  CAstate = {0x10, 0x100, 0x100, 0x100};
+  rng.x = xTaskGetTickCount();
 }
 
 CASim::~CASim() {
   lv_obj_clean(lv_scr_act());
+  lv_task_del(taskRefresh);
+}
+
+//v. slow
+template<typename T>
+T ECA(T board, int rule)
+{
+    T p = board>>1, q = board, r = board<<1, o = 0;
+    for (size_t i = 0; i < sizeof(T)*CHAR_BIT; i++)
+    {
+        T neigh = (((p>>i)&1)<<2) + (((q>>i)&1)<<1) + ((r>>i)&1);
+        o |= ((rule>>neigh)&1)<<i;
+    }
+    return o;
+}
+
+void Pinetime::Applications::Screens::CASim::Refresh() 
+{
+    uint32_t* pixdat = drawbuff.data() + 4; // apparently theresn something wierd in first 16 bytes...
+    constexpr size_t row_stride = (Wcanvas*2)/32;
+    constexpr int shiftsize = 4;
+    constexpr int Hscalefactor = 2;
+
+    if (drawbuff.size()<Hcanvas*row_stride+4) return;
+
+    CAstate.bigmask = ECA<uint8_t>(CAstate.bigmask,54);
+    CAstate.mask1 = ECA<uint16_t>(CAstate.mask1,30);
+    CAstate.mask2 = ECA<uint16_t>(CAstate.mask2,110);
+    CAstate.colca = ECA<uint16_t>(CAstate.mask2, 90);
+
+    //lvgl uses bug endian lmao (cry)
+    for (size_t r = 0; r < Hcanvas; r++)
+    {
+        for (size_t i = 0; i < row_stride-1; i++)
+        {
+            pixdat[row_stride*r + i] = __builtin_bswap32((__builtin_bswap32(pixdat[row_stride*r + i])<<shiftsize) | ((pixdat[row_stride*r + i + 1]>>(8-shiftsize))&((1<<shiftsize)-1)));
+        }
+        size_t ca_idx = r/Hscalefactor;
+        uint32_t newstate = ((CAstate.bigmask>>ca_idx)&1)*( ((CAstate.mask1>>r)&1) + 4*((CAstate.mask2>>r)&1) + 10*((CAstate.colca>>r)&1));
+        pixdat[row_stride*r + row_stride - 1] = __builtin_bswap32((__builtin_bswap32(pixdat[row_stride*r + row_stride - 1])<<shiftsize) | newstate);
+    }
+
+    CAstate.bigmask ^= uint8_t(((rng()&rng()&rng()&rng())>>8)&0xFF);
+    CAstate.mask1 ^= uint16_t(((rng()&rng()&rng()&rng())>>8)&0xFFFF); //low bits supposedly have lower randomness.
+    CAstate.mask2 ^= uint16_t(((rng()&rng()&rng()&rng())>>8)&0xFFFF);
+    CAstate.colca ^= uint16_t(((rng()&rng()&rng()&rng())>>8)&0xFFFF);
+    
+    lv_obj_invalidate(canvas);
+    
+    if (tickrest>0)
+    {
+        tickrest--;
+        return;
+    }
+
+    idx++;
+    if (idx>=text.size())
+    {
+        idx=1;
+    }
+    currword.fill(0);
+    size_t wordstart = idx;
+    while (idx < text.size() && ((idx-wordstart)<=1 || !isspace(text[idx])))
+    {
+        if (idx-wordstart+1<currword.size()) 
+        {
+            currword[idx-wordstart] = text[idx];
+            if (text[idx]=='.')
+            {
+                tickrest += 5;
+            }
+            if (text[idx]==',')
+            {
+                tickrest += 1;
+            }
+        }
+        idx++;
+    }
+    if (idx == text.size()) tickrest += 100;
+
+    lv_label_set_text_static(title, currword.data());
+    lv_label_set_align(title, LV_LABEL_ALIGN_CENTER);
+    lv_obj_align(title, lv_scr_act(), LV_ALIGN_CENTER, 0, 0);
 }
--- src/displayapp/screens/CASim.h
+++ src/displayapp/screens/CASim.h
@@ -4,24 +4,82 @@
 #include "displayapp/screens/Screen.h"
 #include "displayapp/Controllers.h"
 #include "Symbols.h"
+#include "systemtask/WakeLock.h"
+
+#include <array>
+#include <string>
+
+struct fastRNG
+{
+    uint32_t x;
+
+    fastRNG(uint32_t rng_init)
+    {
+        x=rng_init;
+    }
+
+    void next_state()
+    {
+        x ^= x << 13;
+        x ^= x >> 17;
+        x ^= x << 5;
+    }
+    
+    uint32_t operator()()
+    {
+        next_state();
+        return x;
+    }
+};
 
 namespace Pinetime {
   namespace Applications {
     namespace Screens {
       class CASim : public Screen {
       public:
-        CASim();
+        CASim(System::SystemTask* systemTask);
         ~CASim() override;
+
+        void Refresh() override;
+
+        static constexpr std::string_view text = R"( I must not fear. Fear is the mind-killer. Fear is the little-death that brings total obliteration. I will face my fear. I will permit it to pass over me and through me. And when it has gone past I will turn the inner eye to see its path. Where the fear has gone there will be nothing. Only I will remain.)";
+        
+
+      private:
+        lv_task_t* taskRefresh;
+        lv_obj_t* title;
+        lv_obj_t* canvas;
+        static constexpr size_t Wcanvas = 192;
+        static constexpr size_t Hcanvas = 16;
+        static constexpr size_t buffsize = LV_CANVAS_BUF_SIZE_INDEXED_2BIT(Wcanvas,Hcanvas);
+        std::array<uint32_t, (buffsize-1)/4+1> drawbuff;
+
+        std::array<char, 24> currword;
+        size_t idx = 0;
+        int tickrest = 0;
+        Pinetime::System::WakeLock wakeLock;
+        fastRNG rng = {100};
+        
+        struct {
+            uint8_t bigmask;
+            uint16_t mask1;
+            uint16_t mask2;
+            uint16_t colca;
+        } CAstate;
       };
     }
     
     template <>
     struct AppTraits<Apps::CASim> {
       static constexpr Apps app = Apps::CASim;
-      static constexpr const char* icon = Screens::Symbols::eye;
+      static constexpr const char* icon = "R";
       static Screens::Screen* Create(AppControllers& controllers) {
-        return new Screens::CASim();
+        return new Screens::CASim(controllers.systemTask);
       }
+
+      static bool IsAvailable(Pinetime::Controllers::FS& /*filesystem*/) {
+        return true;
+      };
     };
   }
 }
